5cf286719132c69031e6113c34c44b84ed2bcbd7
Michael.k <Mihaill.Nik0laevich@gmail.com>
2019-10-28 17:38:34 +0200

    fixed img iOS

--- webpack.prod.js
+++ webpack.prod.js
@@ -81,28 +81,28 @@ module.exports = merge(common, {
                             outputPath: 'img',
                         },
                     },
-                    {
-                        loader: 'image-webpack-loader',
-                        options: {
-                            mozjpeg: {
-                                progressive: true,
-                                quality: 65
-                            },
-                            optipng: {
-                                enabled: true,
-                            },
-                            pngquant: {
-                                quality: [0.65, 0.90],
-                                speed: 4
-                            },
-                            gifsicle: {
-                                interlaced: false,
-                            },
-                            webp: {
-                                quality: 75
-                            }
-                        }
-                    }
+                    // {
+                    //     loader: 'image-webpack-loader',
+                    //     options: {
+                    //         mozjpeg: {
+                    //             progressive: true,
+                    //             quality: 65
+                    //         },
+                    //         optipng: {
+                    //             enabled: true,
+                    //         },
+                    //         pngquant: {
+                    //             quality: [0.65, 0.90],
+                    //             speed: 4
+                    //         },
+                    //         gifsicle: {
+                    //             interlaced: false,
+                    //         },
+                    //         webp: {
+                    //             quality: 75
+                    //         }
+                    //     }
+                    // }
                 ],
             },
         ],
